de39fe25720f7071bfa2f64645a65b9464ad7dca
Lee Campbell <campbel2@tcd.ie>
2018-11-22 23:09:19 +0000

    github repo request

--- github-webapp/public/js/github-data.js
+++ github-webapp/public/js/github-data.js
@@ -10,11 +10,25 @@ $(document).ready(function() {
     //DOM manipulation code
     getRepos();
 
-
-
-
 });
 
 function getRepos() {
     console.log("Fetching repos");
+
+    $.ajax({
+        url: "https://api.github.com/user/repos",
+        method: "GET",
+        data: {
+            "access_token": sessionStorage.getItem('token')
+        },
+        success: function(repos) {
+            user.repos = repos;
+            sessionStorage.setItem('repos', repos);
+            alert('Success');
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+           console.log(errorThrown);
+           alert('Error');
+        }
+    });
 }
