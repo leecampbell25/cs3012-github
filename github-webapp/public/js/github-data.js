7448c048099a2036463c3d406a0f9746e56dae10
Lee Campbell <campbel2@tcd.ie>
2018-11-23 21:19:53 +0000

    getContributors working

--- github-webapp/public/js/github-data.js
+++ github-webapp/public/js/github-data.js
@@ -2,7 +2,7 @@ var user = {
     username: sessionStorage.getItem('user'),
     data: null,
     repos: null,
-    contributors: null,
+    contributors: [],
     thumbnail: null
 }
 
@@ -37,7 +37,6 @@ function populateUserData() {
 }
 
 function getAuthUserRepos() {
-    console.log("Fetching repos");
 
     $.ajax({
         url: "https://api.github.com/user/repos",
@@ -47,10 +46,73 @@ function getAuthUserRepos() {
         },
         success: function(repos) {
             user.repos = repos;
+            console.log("got repos");
+            getContributors(user);
         },
         error: function(jqXHR, textStatus, errorThrown) {
            console.log(errorThrown);
            alert('Error');
         }
     });
+}
+
+function getContributors(user) {
+  console.log("get contributors");
+  for(var i = 0; i < user.repos.length; i++) {
+
+    var repo = user.repos[i];
+
+    $.ajax({
+        url: repo.contributors_url,
+        method: "GET",
+        data: {
+            "access_token": sessionStorage.getItem('token')
+        },
+        success: function(contributors) {
+
+            if (contributors == null)
+            {
+              return;
+            }
+
+            else
+            {
+
+              for (var j = 0; j < contributors.length; j++) {
+
+                var contributor = {
+                  username: contributors[j].login,
+                  repos: null,
+                  contributors: [],
+                  thumbnail: contributors[j].avatar_url,
+                }
+
+                if (isNewContributor(contributor.username, user.contributors)
+              && contributor.username != user.username){
+                   user.contributors.push(contributor);
+                   console.log("new contributor pushed");
+                }
+              }
+
+            }
+
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+           console.log(errorThrown);
+           alert('Error');
+        }
+
+    });
+
+  }
+
+}
+
+function isNewContributor(username, contributors) {
+console.log("new con");
+for (var i = 0; i < contributors.length; i++) {
+     if (username === contributors[i].username)
+         return false;
+       }
+ return true;
 }
